ceb79bb6a64dca2cb9c24f050371c8af4649d969
Lucas Vilas Boas <lucasvilasboaslage@gmail.com>
2025-09-21 20:54:16 -0300

    feat: creating new event order-qualified

--- services/eventify-qualification-service/src/qualification/qualification.service.ts
+++ services/eventify-qualification-service/src/qualification/qualification.service.ts
@@ -9,5 +9,51 @@ export class QualificationService {
     this.logger.log(
       `Processing order - ${data.orderId}: ${JSON.stringify(data)}`,
     );
+
+    const isCapital = this.isCapital(data.deliveryAddress.city);
+
+    if (isCapital) {
+      this.logger.log(
+        `Order ${data.orderId} is from a capital city: ${data.deliveryAddress.city}`,
+      );
+    } else {
+      this.logger.log(
+        `Order ${data.orderId} is NOT from a capital city: ${data.deliveryAddress.city}`,
+      );
+    }
+  }
+
+  isCapital(city: string): boolean {
+    const capitals = [
+      'Rio Branco',
+      'Maceió',
+      'Macapá',
+      'Manaus',
+      'Salvador',
+      'Fortaleza',
+      'Brasília',
+      'Vitória',
+      'Goiânia',
+      'São Luís',
+      'Cuiabá',
+      'Campo Grande',
+      'Belo Horizonte',
+      'Belém',
+      'João Pessoa',
+      'Curitiba',
+      'Recife',
+      'Teresina',
+      'Rio de Janeiro',
+      'Natal',
+      'Porto Alegre',
+      'Porto Velho',
+      'Boa Vista',
+      'Florianópolis',
+      'São Paulo',
+      'Aracaju',
+      'Palmas',
+    ];
+
+    return capitals.includes(city);
   }
 }
