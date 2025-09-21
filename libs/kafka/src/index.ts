2152fe507b95a3cfbce5d9b2abc5ee8e50154b3b
Lucas Vilas Boas <lucasvilasboaslage@gmail.com>
2025-09-21 20:58:13 -0300

    fix: import type of OrderQualifiedEvetn

--- libs/kafka/src/index.ts
+++ libs/kafka/src/index.ts
@@ -2,4 +2,5 @@
 export * from "./kafka.module";
 export * from "./kafka.service";
 export * from "./events/order-created";
+export * from "./events/order-qualified";
 export * from "./constants/kafka.constant";
