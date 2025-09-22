b5df2eb18c1c7d0ed3c669a3b80b7cd887903f8e
Lucas Vilas Boas <lucasvilasboaslage@gmail.com>
2025-09-21 21:17:10 -0300

    feat: adding dlq support to event order-qualified

--- libs/kafka/src/events/order-qualified.ts
+++ libs/kafka/src/events/order-qualified.ts
@@ -1,4 +1,5 @@
 export const ORDER_QUALIFIED_EVENT = "order-qualified";
+export const ORDER_QUALIFIED_DLQ_EVENT = "order-qualified-dlq";
 export interface OrderQualifiedEvent {
   orderId: string;
   qualified: boolean;
