b5b622e9869f4045347a4353af113300dbea4dad
Lucas Vilas Boas <lucasvilasboaslage@gmail.com>
2025-09-28 19:11:15 -0300

    feature: grouping products by Id

--- services/eventify-order-service/src/orders/useCases/create-order.use-case.ts
+++ services/eventify-order-service/src/orders/useCases/create-order.use-case.ts
@@ -1,5 +1,5 @@
 import { Injectable, Logger } from '@nestjs/common';
-import { CreateOrderRequest } from '../contracts/create-order.contract';
+import { CreateOrderRequest, ProductItemRequest } from '../contracts/create-order.contract';
 import { OrderRepository } from '../repository/orders.repository';
 import { KafkaService, ORDER_CREATED_EVENT, OrderCreatedEvent } from '@vilasboas1407/kafka';
 import { Order } from '../schemas/order.schema';
@@ -7,24 +7,40 @@ import { Order } from '../schemas/order.schema';
 @Injectable()
 export class CreateOrderUseCase {
   private readonly logger = new Logger(CreateOrderUseCase.name);
+
   constructor(
     private readonly orderRepository: OrderRepository,
     private readonly kafkaService: KafkaService
   ) {}
 
   async execute(request: CreateOrderRequest): Promise<string> {
+    request.items = this.groupOrderItems(request.items);
+
     const order = await this.orderRepository.create(request);
+    this.logger.log(`Order created with ID: ${order.id}`);
 
     const message = this.mapToOrderCreatedEvent(order);
-
     try {
-      this.logger.log(`Sending Kafka event: ${JSON.stringify(message)}`);
+      this.logger.log(`Sending ORDER_CREATED_EVENT to Kafka: ${JSON.stringify(message)}`);
       await this.kafkaService.sendMessage(ORDER_CREATED_EVENT, message);
+      this.logger.log(`Kafka event ORDER_CREATED_EVENT sent successfully for order ${order.id}`);
     } catch (err) {
-      this.logger.error(`Failed to send Order Created - Order Id ${order} event: ${err.message}`);
+      this.logger.error(`Failed to send Kafka event for order ${order.id}: ${err.message}`);
     }
 
     return order.id;
+  }
+
+  private groupOrderItems(items: ProductItemRequest[]) {
+    const map = new Map<string, number>();
+    for (const item of items) {
+      if (map.has(item.productId)) {
+        map.set(item.productId, map.get(item.productId)! + item.count);
+      } else {
+        map.set(item.productId, item.count);
+      }
+    }
+    return Array.from(map.entries()).map(([productId, count]) => ({ productId, count }));
   }
 
   private mapToOrderCreatedEvent(order: Order): OrderCreatedEvent {
@@ -32,7 +48,7 @@ export class CreateOrderUseCase {
       orderId: order.id,
       customerEmail: order.customer.email,
       customerId: order.customer.id,
-      customerName: order.customer.firstName + ' ' + order.customer.lastName,
+      customerName: `${order.customer.firstName} ${order.customer.lastName}`,
       items: order.items.map((item) => ({ productId: item.productId, count: item.count })),
       deliveryAddress: order.customer.deliveryAddress,
       createdAt: order.createdAt.toISOString(),
