902472094c427880c42e0148bc0684f53da102b7
Lucas Vilas Boas <lucasvilasboaslage@gmail.com>
2025-10-21 21:05:30 -0300

    feat: adding logging

--- services/eventify-inventory-service/src/inventory/services/product.service.ts
+++ services/eventify-inventory-service/src/inventory/services/product.service.ts
@@ -1,9 +1,10 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Injectable, Logger, NotFoundException } from '@nestjs/common';
 import { ProductRepository } from '../repository/product.repository';
 import { Product } from '../schema/product.schema';
 
 @Injectable()
 export class ProductService {
+  private readonly logger = new Logger(ProductService.name);
   constructor(private readonly productRepository: ProductRepository) {}
 
   async getAll(): Promise<Product[]> {
@@ -11,17 +12,24 @@ export class ProductService {
   }
 
   async getById(id: string): Promise<Product> {
+    this.logger.log(`Fetching product with ID: ${id}`);
     const product = await this.productRepository.findById(id);
-    if (!product)
+    if (!product) {
+      this.logger.error(`Product with ID: ${id} not found`);
       throw new NotFoundException(`Product with id ${id} not found`);
+    }
     return product;
   }
 
   async findByIds(ids: string[]): Promise<Product[]> {
+    this.logger.log(`Fetching products with IDs: ${ids.join(', ')}`);
     return this.productRepository.findByIds(ids);
   }
 
   async updateStock(productId: string, quantity: number): Promise<void> {
+    this.logger.log(
+      `Updating stock for product ID: ${productId} by quantity: ${quantity}`,
+    );
     const product = await this.getById(productId);
     product.stock += quantity;
     await this.productRepository.update(product);
